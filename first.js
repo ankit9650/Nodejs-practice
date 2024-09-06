d17c0bc11cd444938e5fce4fae2d9314a45f6ec5
ankit9650 <ankitprabhaker9650@gmail.com>
2024-09-06 14:03:34 +0530

    log creating

--- first.js
+++ first.js
@@ -2,9 +2,9 @@ var http = require('http')
 var Date = require('./module1')
 var fs = require('fs')
 http.createServer(function(req,res){
-    res.writeHead(200);
+    //res.writeHead(200);
     //################ import module ################//
-    res.end('hello ankit'+Date.myDateTime())
+    //res.end('hello ankit'+Date.myDateTime())
     // console.log('hii ankit')  
 
     //################ file write and create ######################//
@@ -15,4 +15,12 @@ http.createServer(function(req,res){
 
     // const Reading= fs.readFileSync("./file1.txt","utf-8")    
     // console.log(Reading)    
-}).listen(8080)
+    //################ update file on every request ######################//
+    const log = `${Date.myDateTime()}: Record added \n`
+    fs.appendFile("./file.txt",log,(err, data)=>{
+        res.end("response done")
+    })
+
+}).listen(8080, ()=>{
+    console.log("server started")
+})
