07dc10441789c6be137657506b4aeff64f5d6e9a
ankit9650 <ankitprabhaker9650@gmail.com>
2024-09-17 19:46:50 +0530

    mockdata added and used

--- express.js
+++ express.js
@@ -1,10 +1,25 @@
 
 var express = require('express')
+var users = require('./MOCK_DATA.json')
 var app= express();
 
-app.get("/",(req,res)=>{
-   
-    return res.send(`hey ankit${req.query.myname}`);
+app.get("/users",(req,res)=>{   
+    return res.json(users);
+})
+
+app.get("/users/:id", (req, res) => {
+    var id = Number(req.params.id);    
+    var user = users.find((user) => user.id === id);  
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  });
+
+app.post("/users",(res,req)=>{
+
+    return res.json({status:"pending"})
 })
 
 app.listen(8080,()=>{
